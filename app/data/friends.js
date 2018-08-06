72c509dca91be0f3d117551ad994cf831631a683
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-08-05 21:33:07 -0500

    General file prep

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,56 @@
-//  DATA
-// The Data below holds previous FriendFinder Survey results.
-// Placeholder data below is what we will evaluate our demo data against.
-
 var friendsArray = [
     {
-        name: George,
+        name: "George of the Jungle",
         photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvignette.wikia.nocookie.net%2Fdisney%2Fimages%2F6%2F64%2FGeorge_of_the_Jungle_Waving.jpg%2Frevision%2Flatest%3Fcb%3D20120406072510&imgrefurl=http%3A%2F%2Fdisney.wikia.com%2Fwiki%2FFile%3AGeorge_of_the_Jungle_Waving.jpg&docid=9kD5G8HVWL6XMM&tbnid=Yv65F0xq7Lp6OM%3A&vet=10ahUKEwj1k_XL-9TcAhVIj1QKHfw3BqMQMwj3ASgFMAU..i&w=399&h=523&bih=582&biw=1279&q=george%20of%20the%20jungle&ved=0ahUKEwj1k_XL-9TcAhVIj1QKHfw3BqMQMwj3ASgFMAU&iact=mrc&uact=8",
         scores: [1,3,2,4,5,4,4,1,3,2]
+    },
+    {
+        name: "Buffy Summers",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi8y5rXqNfcAhVp64MKHZyYAXUQjRx6BAgBEAU&url=http%3A%2F%2Fwarner-bros-characters.wikia.com%2Fwiki%2FBuffy_Summers&psig=AOvVaw1f1ttGv1bV9zzrihTtDkuC&ust=1533606648131248",
+        scores: [2,5,3,2,5,1,3,2,5]
+    },
+    {
+        name: "Data",
+        photo: "http://www.startrek.com/uploads/assets/db_articles/e81c754bada3cc253e296896254bbe73916ac363.jpg",
+        scores: [4,3,1,3,3,4,1,1,1,3]
+    },
+    {
+        name: "The Doctor",
+        photo: "https://cdn3.whatculture.com/images/2013/03/matt-smith_doctor_who11.jpg",
+        scores: [5,5,1,5,1,5,1,1,4,5]
+    },
+    {
+        name: "Rick Sanchez",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest/scale-to-width-down/620?cb=20160923150728",
+        scores: [1,1,1,3,1,1,1,5,1,5]
+    },
+    {
+        name: "Leslie Knope",
+        photo: "https://tribzap2it.files.wordpress.com/2016/11/amy-poehler-leslie-knope-parks-and-recreation.jpg",
+        scores: [5,5,1,5,1,5,1,1,4,5]
+    },
+    {
+        name: "Terry Jeffords",
+        photo: "http://reductress.com/wp-content/uploads/2018/06/terry-crews.png",
+        scores: [2,3,2,2,5,4,1,5,1,2]
+    },
+    {
+        name: "Hayao Miyazaki",
+        photo: "https://i.kym-cdn.com/photos/images/newsfeed/000/668/930/96b.jpg",
+        scores: [4,2,1,4,2,5,3,2,5,5]
+    },
+    {
+        name: "Gary Gygax",
+        photo: "http://cdn.themis-media.com/media/global/images/galleries/display/47/47451.jpg",
+        scores: [4,4,4,3,2,5,1,4,5,5]
+    },
+    {
+        name: "Peter Parker",
+        photo: "https://orig00.deviantart.net/d423/f/2017/211/1/e/a_night_to_remember__peter_parker_x_reader__2_3_by_savrom-dbi8qtv.jpg",
+        scores: [4,3,5,4,4,3,1,3,1,2]
     }
 ];
 
-//This Module Also Exports Bands Object 
+//This Module Also Exports Friends Object 
 module.exports = friendsArray;
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,18 @@
-// LOAD DATA
-// Link our routes to the Friends.js data arrays.
-
+// Dependencies
+var path = require('path');
 var friendsData = require("../data/friends");
 
 // ROUTING
 
 //  API GET request will display friendsData when the usser visits the page.
 module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
-     res.jason(friendsData);   
+    app.get("/data/friends", function(req, res) {
+     res.json(friendsData);   
    });
 
 // API POST request submit survey data into our friends.js friendsArray.
-   app.post("/api/friends", function(req, res) {
+   app.post("/data/friends", function(req, res) {
      friendsData.push(req.body);
 // Insert logic to find best friend match.
    });
-}
-
-//This Module Also Exports Bands Object 
-module.exports.friends = {
-	friends
 }
