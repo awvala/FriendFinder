3040599e9e94c08d2d5ac21b83fb8a2cfd978422
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-08-06 00:21:46 -0500

    corrected friendsData ref2

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
 // Dependencies
-var friendsData = require("../data/friends.js");
+var friends = require('../data/friends.js');
 
 // ROUTING
-//  API GET request will display friendsData when the usser visits the page.
-module.exports = function (app) {
-  app.get("/api/friends", function (req, res) {
-    res.json(friendsData);
-  });
+//  API GET request will display friends when the user visits the page.
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friends);
+	});
 
   // API POST request submit survey data into our friends.js friendsArray.
   app.post("/api/friends", function (req, res) {
@@ -19,21 +19,21 @@ module.exports = function (app) {
     var friendImg = '';
     var totalDiff = 100;
 
-    for (var i = 0; i < friendsData.lengh; i++) {
+    for (var i = 0; i < friends.lengh; i++) {
       
       var diff = 0;
       for (j = 0; j < userAnswers.lengh; j++) {
-        diff += Math.abs(friendsData[i].scores[j] - userAnswers[j]);
+        diff += Math.abs(friends[i].scores[j] - userAnswers[j]);
       }
       
       // if lowest mathematical difference, update friend variables
       if (diff < totalDiff) {
         totalDiff = diff;
-        friendName = friendsData[i].name;
-        friendImg = friendsData[i].photo;
+        friendName = friends[i].name;
+        friendImg = friends[i].photo;
       }
     }
-    friendsData.push(userData);
+    friends.push(userData);
     res.json({status: 'ok', name: friendName, photo: friendImg});
   });
 };
